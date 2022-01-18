11f56f3b6d9336d12b4cf885826961286a65930c
Yuvraj Singh Birdi <96745820+yuvrajbirdi@users.noreply.github.com>
2022-01-18 16:09:10 +0530

    Update and rename Assignment2_answers.py to Assignment2_Answers.py

--- Assignment2_Answers.py
+++ Assignment2_Answers.py
@@ -1,8 +1,3 @@
-Python 3.9.5 (v3.9.5:0a7dcbdb13, May  3 2021, 13:17:02) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> /usr/local/bin/python3 /Users/uschiemaun/Desktop/Assingment2_questions.py
-(base) uschiemaun@Yuvrajs-MacBook-Air ~ % /usr/local/bin/python3 /Users/uschiemaun/Desktop/Assingment2_questions.py
 Answer 1
 a. 35
 b. egaugnal evitisnes esac a si nohtyP
