c0267cd071244ced2cb24b010c8f8c0daffef83c
Yuvraj Singh Birdi <96745820+yuvrajbirdi@users.noreply.github.com>
2022-02-27 10:51:36 +0530

    Add files via upload

---
+++ Assignment4_Answers.py
@@ -0,0 +1,47 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+Question 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+Question 2
+Enter number upto which you want to print Pascal's Triangle:5
+Using recursion:
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+Using Iteration:
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+Question 3
+Enter two values a and b separated by single space:5 2
+Quotient and remainder for entered value is: (2, 1)
+<A> Is function callable: False
+<B> Are entered values nonzero: Yes
+<C> Adding (4,5,6) to it: (2, 1, 4, 5, 6) and after filtering values > 4: (2, 1, 4)
+<D> Converting to set datatype: {1, 2, 4}
+<E> Making the set immutable: frozenset({1, 2, 4})
+<E> Maximum value from set is: 4 and its hash value is: 4
+Question 4
+Enter your name:Yuvraj Singh Birdi
+Enter your SID:21103049
+Name is Yuvraj Singh Birdi and SID is 21103049
+Destroyed created object
+Question 5
+Name is Mehak and salary is 40000
+Name is Ashok and salary is 50000
+Name is Viren and salary is 60000
+<A> Updated:Name is Mehak and salary is 70000
+<B> Record of Viren deleted.
+Question 6
+Enter the word uttered by George:hello
+Word created by Barbie:hlloe
